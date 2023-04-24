1e4f75c415c7fcea191fd70c250b7d971087820c
[Github <beomsuong0406@gmail.com>
2023-04-24 23:22:41 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -95,7 +95,7 @@ class mypage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
+      body: ListView(
         children: <Widget>[
           const Align(
             alignment: Alignment.bottomLeft,
@@ -133,81 +133,209 @@ class mypage extends StatelessWidget {
               ),
             ],
           ),
+          Padding(
+            padding: const EdgeInsets.only(right: 30, left: 30),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: const [
+                SizedBox(
+                  child: Text(
+                    '기본 정보',
+                    style: TextStyle(fontSize: 15, fontWeight: FontWeight.w700),
+                  ),
+                ),
+                SizedBox(
+                  child: Text(
+                    '수정버튼 수정 예정',
+                    style: TextStyle(fontSize: 15, fontWeight: FontWeight.w700),
+                  ),
+                ),
+              ],
+            ),
+          ),
           Align(
             alignment: Alignment.bottomLeft,
             child: Padding(
-              padding: const EdgeInsets.only(right: 30, left: 30),
+              padding: const EdgeInsets.only(right: 25, left: 25),
               child: Column(
                 children: [
-                  const Text(
-                    '이름 123예정',
+                  const Divider(
+                    thickness: 2,
+                    height: 3,
+                    color: Colors.black,
+                  ),
+                  SizedBox(
+                    height: 30,
+                    child: Row(
+                      children: const [
+                        Text(
+                          '대학 :',
+                          style: TextStyle(
+                              fontSize: 15, fontWeight: FontWeight.w700),
+                        ),
+                        Text(
+                          '대학교 변수',
+                          style: TextStyle(
+                              fontSize: 15, fontWeight: FontWeight.w700),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    height: 30,
+                    child: Row(
+                      children: const [
+                        Text(
+                          '학과 :',
+                          style: TextStyle(
+                              fontSize: 15, fontWeight: FontWeight.w700),
+                        ),
+                        Text(
+                          '학과 변수',
+                          style: TextStyle(
+                              fontSize: 15, fontWeight: FontWeight.w700),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    height: 30,
+                    child: Row(
+                      children: const [
+                        Text(
+                          'MBTI :',
+                          style: TextStyle(
+                              fontSize: 15, fontWeight: FontWeight.w700),
+                        ),
+                        Text(
+                          'MBTI 변수',
+                          style: TextStyle(
+                              fontSize: 15, fontWeight: FontWeight.w700),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    height: 30,
+                    child: Row(
+                      children: const [
+                        Text(
+                          '연락 가능 시간 :',
+                          style: TextStyle(
+                              fontSize: 15, fontWeight: FontWeight.w700),
+                        ),
+                        Text(
+                          '연락가능 시간 변수',
+                          style: TextStyle(
+                              fontSize: 15, fontWeight: FontWeight.w700),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const Divider(
+                    thickness: 2,
+                    height: 10,
+                    color: Colors.black,
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(right: 30, left: 30, top: 30),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: const [
+                SizedBox(
+                  child: Text(
+                    '활동 이력',
                     style: TextStyle(fontSize: 15, fontWeight: FontWeight.w700),
                   ),
+                ),
+                SizedBox(
+                  child: Text(
+                    '조회 버튼 추가 예정',
+                    style: TextStyle(fontSize: 15, fontWeight: FontWeight.w700),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomLeft,
+            child: Padding(
+              padding: const EdgeInsets.only(right: 25, left: 25),
+              child: Column(
+                children: [
+                  const Divider(
+                    thickness: 2,
+                    height: 3,
+                    color: Colors.black,
+                  ),
+                  SizedBox(
+                    height: 30,
+                    child: Row(
+                      children: const [
+                        Text(
+                          '현재 참여중인 과제 : ',
+                          style: TextStyle(
+                              fontSize: 15, fontWeight: FontWeight.w700),
+                        ),
+                        Text(
+                          '갯수 변수',
+                          style: TextStyle(
+                              fontSize: 15, fontWeight: FontWeight.w700),
+                        ),
+                        Text(
+                          '(개)',
+                          style: TextStyle(
+                              fontSize: 15, fontWeight: FontWeight.w700),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    height: 30,
+                    child: Row(
+                      children: const [
+                        Text(
+                          '완료한 과제 : ',
+                          style: TextStyle(
+                              fontSize: 15, fontWeight: FontWeight.w700),
+                        ),
+                        Text(
+                          '갯수 변수',
+                          style: TextStyle(
+                              fontSize: 15, fontWeight: FontWeight.w700),
+                        ),
+                        Text(
+                          '(개)',
+                          style: TextStyle(
+                              fontSize: 15, fontWeight: FontWeight.w700),
+                        ),
+                      ],
+                    ),
+                  ),
                   const Divider(
                     thickness: 2,
                     height: 10,
                     color: Colors.black,
                   ),
-                  Row(
-                    children: const [
-                      Text(
-                        '대학 :',
-                        style: TextStyle(
-                            fontSize: 15, fontWeight: FontWeight.w700),
-                      ),
-                      Text(
-                        '대학교 변수',
-                        style: TextStyle(
-                            fontSize: 15, fontWeight: FontWeight.w700),
-                      ),
-                    ],
-                  ),
-                  Row(
-                    children: const [
-                      Text(
-                        '학과 :',
-                        style: TextStyle(
-                            fontSize: 15, fontWeight: FontWeight.w700),
-                      ),
-                      Text(
-                        '학과 변수',
-                        style: TextStyle(
-                            fontSize: 15, fontWeight: FontWeight.w700),
-                      ),
-                    ],
-                  ),
-                  Row(
-                    children: const [
-                      Text(
-                        'MBTI :',
-                        style: TextStyle(
-                            fontSize: 15, fontWeight: FontWeight.w700),
-                      ),
-                      Text(
-                        'MBTI 변수',
-                        style: TextStyle(
-                            fontSize: 15, fontWeight: FontWeight.w700),
-                      ),
-                    ],
-                  ),
-                  Row(
-                    children: const [
-                      Text(
-                        '연락 가능 시간 :',
-                        style: TextStyle(
-                            fontSize: 15, fontWeight: FontWeight.w700),
-                      ),
-                      Text(
-                        '연락가능 시간 변수',
-                        style: TextStyle(
-                            fontSize: 15, fontWeight: FontWeight.w700),
-                      ),
-                    ],
-                  ),
-                  const Divider(
-                    thickness: 2,
-                    height: 10,
-                    color: Colors.black,
+                ],
+              ),
+            ),
+          ),
+          SizedBox(
+            child: SizedBox(
+              child: Column(
+                children: const [
+                  Padding(
+                    padding: EdgeInsets.only(top: 7, left: 7),
+                    child: Text(
+                      'Level : ' '레벨변수',
+                      style:
+                          TextStyle(fontSize: 25, fontWeight: FontWeight.w900),
+                    ),
                   ),
                 ],
               ),
