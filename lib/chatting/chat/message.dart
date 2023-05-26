d5629c333ecbdb88a5cc6d07453ad1c3d95a09a0
IndiaInk10 <indiaink10@gmail.com>
2023-05-27 06:23:24 +0900

    유저 이름 변경시 적용되도록 수정
    message.dart
    - readUserName 으로 One-time 읽기 + 반복되는 읽기 방지를 위한 Map 활용
    
    * 현재 처음 Stream 넘어갈때 불러오는 시간 때문에 유저 이름이 바로 안 뜨는 문제가 존재
    * 추후에 톡방에 참가자 목록 넣으면 손쉽게 해결됨

--- lib/chatting/chat/message.dart
+++ lib/chatting/chat/message.dart
@@ -3,23 +3,38 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'chat_bubble.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
-class Messages extends StatelessWidget {
+class Messages extends StatefulWidget {
   final String roomname;
   Messages({Key? key, required this.roomname}) : super(key: key);
+
+  @override
+  State<Messages> createState() => _MessagesState();
+}
+
+class _MessagesState extends State<Messages> {
+  late final CollectionReference userCollectionRef;
+  late final Stream messageCollectionStream;
+  Map<String, String> userMap = {};
+
+  @override
+  void initState() {
+    super.initState();
+    userCollectionRef = FirebaseFirestore.instance.collection('exuser');
+    messageCollectionStream = FirebaseFirestore.instance
+        .collection('exchat')
+        .doc(widget.roomname)
+        .collection('message')
+        .orderBy('time', descending: true)
+        .snapshots();
+  }
 
   @override
   Widget build(BuildContext context) {
     final user = FirebaseAuth.instance.currentUser;
 
     return StreamBuilder(
-      stream: FirebaseFirestore.instance
-          .collection('exchat')
-          .doc(roomname)
-          .collection('message')
-          .orderBy('time', descending: true)
-          .snapshots(),
-      builder: (context,
-          AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
+      stream: messageCollectionStream,
+      builder: (context, snapshot) {
         if (snapshot.connectionState == ConnectionState.waiting) {
           return Center(
             child: CircularProgressIndicator(),
@@ -31,14 +46,30 @@ class Messages extends StatelessWidget {
           reverse: true,
           itemCount: chatDocs.length,
           itemBuilder: (context, index) {
+            String userID = chatDocs[index]['userID'];
+            userMap.containsKey(userID)
+                ? null
+                : readUserName(userID)
+                    .then((userName) => storeUserName(userID, userName));
             return ChatBubbles(
                 chatDocs[index]['text'],
                 chatDocs[index]['userID'].toString() == user!.uid,
-                chatDocs[index]['userName'],
+                userMap[userID] ?? 'userName',
                 chatDocs[index]['userImage']);
           },
         );
       },
     );
+  }
+
+  void storeUserName(String userID, String userName) => setState(() {
+        userMap[userID] = userName;
+      });
+  Future<String> readUserName(String userID) async {
+    var docSnapshot = await userCollectionRef.doc(userID).get();
+    if (docSnapshot.exists) {
+      return docSnapshot.get('이름');
+    }
+    return '알수없음';
   }
 }
