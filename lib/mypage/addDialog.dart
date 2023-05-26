ea0ebe6e7c0e8402d37a6c536e5d3b4d972d1cee
[Github <beomsuong0406@gmail.com>
2023-05-26 23:45:01 +0900

    정보수정

--- lib/mypage/addDialog.dart
+++ lib/mypage/addDialog.dart
@@ -1,7 +1,10 @@
+import 'package:capston/mypage/ModifyDialog.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class AddDialog extends StatefulWidget {
-  final String university, major, mbti, contacttime;
+  late String university, major, mbti, contacttime;
   AddDialog({
     Key? key,
     required this.university,
@@ -9,41 +12,219 @@ class AddDialog extends StatefulWidget {
     required this.mbti,
     required this.contacttime,
   }) : super(key: key);
-
   @override
   State<AddDialog> createState() => _AddDialog1State();
 }
 
-//요일을 int로 변환하기 위한 map
+@override
+void initState() {}
+
+Future<DocumentSnapshot> loadingdata() async {
+  final authentication = FirebaseAuth.instance;
+
+  final user = authentication.currentUser;
+  print(user!.uid);
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
+
+  // 최상위 컬렉션에서 하위 컬렉션까지 한 번에 지정하는 변수
+  DocumentReference docRef = firestore.collection('exuser').doc(user.uid);
+
+  // 문서의 데이터를 가져옵니다.
+  DocumentSnapshot docSnapshot = await docRef.get();
+
+  return docSnapshot;
+}
+
 class _AddDialog1State extends State<AddDialog> {
   @override
   Widget build(BuildContext context) {
-    return AlertDialog(
-      title: Text('수정'),
-      content: SingleChildScrollView(
-        child: ListBody(
-          children: <Widget>[
-            Row(children: [Text(widget.university), Text(widget.university)]),
-            Text(widget.major),
-            Text(widget.mbti),
-            Text(widget.contacttime),
-          ],
-        ),
+    return Dialog(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(Radius.circular(30.0)),
       ),
-      actions: <Widget>[
-        TextButton(
-          child: Text('ok'),
-          onPressed: () {
-            Navigator.of(context).pop();
-          },
-        ),
-        TextButton(
-          child: Text('cancel'),
-          onPressed: () {
-            Navigator.of(context).pop();
-          },
-        ),
-      ],
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.purple,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(30.0),
+                topRight: Radius.circular(30.0),
+              ),
+            ),
+            padding: EdgeInsets.only(left: 15.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text("수정하기",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w500,
+                        fontSize: 20)),
+                IconButton(
+                    color: Colors.white,
+                    iconSize: 30,
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                      setState(() {});
+                    },
+                    icon: Icon(Icons.check))
+              ],
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(left: 15.0, right: 15),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(widget.university,
+                    style:
+                        TextStyle(fontWeight: FontWeight.w500, fontSize: 20)),
+                ElevatedButton(
+                  onPressed: () {
+                    showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return ModifyDialog(
+                          returndata: (String value) {
+                            widget.university = value;
+                            setState(() {});
+                          },
+                          university: widget.university,
+                          datatype: "대학",
+                        );
+                      },
+                    );
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor:
+                        Color.fromARGB(255, 148, 61, 255), // 버튼 배경색 지정
+                  ),
+                  child: Text(
+                    '변경',
+                    style: TextStyle(fontSize: 15, fontWeight: FontWeight.w700),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            height: 1.0,
+            width: 500.0,
+            color: Colors.purple,
+          ),
+          Padding(
+            padding: EdgeInsets.only(left: 15.0, right: 15),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(widget.major,
+                    style:
+                        TextStyle(fontWeight: FontWeight.w500, fontSize: 20)),
+                ElevatedButton(
+                  onPressed: () {
+                    showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return ModifyDialog(
+                          returndata: (String value) {
+                            widget.major = value;
+                            setState(() {});
+                          },
+                          university: widget.major,
+                          datatype: "학과",
+                        );
+                      },
+                    );
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor:
+                        Color.fromARGB(255, 148, 61, 255), // 버튼 배경색 지정
+                  ),
+                  child: Text(
+                    '변경',
+                    style: TextStyle(fontSize: 15, fontWeight: FontWeight.w700),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            height: 1.0,
+            width: 500.0,
+            color: Colors.purple,
+          ),
+          Padding(
+            padding: EdgeInsets.only(left: 15.0, right: 15),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(widget.mbti,
+                    style:
+                        TextStyle(fontWeight: FontWeight.w500, fontSize: 20)),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor:
+                        Color.fromARGB(255, 148, 61, 255), // 버튼 배경색 지정
+                  ),
+                  child: Text(
+                    '변경',
+                    style: TextStyle(fontSize: 15, fontWeight: FontWeight.w700),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            height: 1.0,
+            width: 500.0,
+            color: Colors.purple,
+          ),
+          Padding(
+            padding: EdgeInsets.only(left: 15.0, right: 15),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(widget.contacttime,
+                    style:
+                        TextStyle(fontWeight: FontWeight.w500, fontSize: 20)),
+                ElevatedButton(
+                  onPressed: () {
+                    showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return ModifyDialog(
+                          returndata: (String value) {
+                            widget.contacttime = value;
+                            setState(() {});
+                          },
+                          university: widget.contacttime,
+                          datatype: "연락가능시간",
+                        );
+                      },
+                    );
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor:
+                        Color.fromARGB(255, 148, 61, 255), // 버튼 배경색 지정
+                  ),
+                  child: Text(
+                    '변경',
+                    style: TextStyle(fontSize: 15, fontWeight: FontWeight.w700),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 30,
+          )
+        ],
+      ),
     );
   }
 }
