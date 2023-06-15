7ca4b3cfd7780765d243721ab769d2495ea7f4bb
IndiaInk10 <indiaink10@gmail.com>
2023-06-15 22:20:15 +0900

    Del .env setting

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,14 @@
 import 'package:capston/chatting/main_screen.dart';
 import 'package:capston/palette.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_config/flutter_config.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
+import 'package:capston/firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await FlutterConfig.loadEnvVariables(); //추가
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  FlutterConfig.get('apiKey');
   runApp(
     const MyApp(),
   );
@@ -23,6 +20,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'CourseMic',
       theme: ThemeData(primarySwatch: Palette.primary),
       home: const LoginSignupScreen(),
