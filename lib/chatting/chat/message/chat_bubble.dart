52fe854a363fc203c956c735901cb8bba98adac9
Park-SangWook <qkrtkddnr0118@gmai.com>
2023-06-18 22:44:36 +0900

    in_progress

--- lib/chatting/chat/message/chat_bubble.dart
+++ lib/chatting/chat/message/chat_bubble.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_chat_bubble/chat_bubble.dart';
 import 'package:fluttertoast/fluttertoast.dart';
+import 'package:intl/intl.dart';
 import 'save_important_message.dart';
 
 class ChatBubbles extends StatelessWidget {
@@ -31,6 +32,12 @@ class ChatBubbles extends StatelessWidget {
     child: const Text("해당 채팅이 클립보드에 복사되었습니다",
         style: TextStyle(color: Colors.white)),
   );
+
+  String getFormattedTime() {
+    final DateTime dateTime = sendTime.toDate();
+    final DateFormat formatter = DateFormat('HH:mm');
+    return formatter.format(dateTime);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -109,68 +116,98 @@ class ChatBubbles extends StatelessWidget {
           mainAxisAlignment:
               isMe ? MainAxisAlignment.end : MainAxisAlignment.start,
           children: [
-            if (isMe)
-              Padding(
-                padding: const EdgeInsets.fromLTRB(0, 10, 45, 0),
-                child: ChatBubble(
-                  clipper: ChatBubbleClipper8(type: BubbleType.sendBubble),
-                  alignment: Alignment.topRight,
-                  margin: const EdgeInsets.only(bottom: 10),
-                  backGroundColor: const Color(0xFF8754f8),
-                  child: Container(
-                    constraints: BoxConstraints(
-                      maxWidth: MediaQuery.of(context).size.width * 0.7,
-                    ),
-                    child: Column(
-                      crossAxisAlignment: isMe
-                          ? CrossAxisAlignment.end
-                          : CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          userName,
-                          style: const TextStyle(
-                              fontWeight: FontWeight.bold, color: Colors.white),
+            if (isMe) //! 나일 때
+              Row(
+                children: [
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Text(
+                        getFormattedTime(),
+                        style: const TextStyle(
+                            fontSize: 10, color: Palette.darkGray),
+                      ),
+                    ],
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(0, 10, 45, 0),
+                    child: ChatBubble(
+                      clipper: ChatBubbleClipper8(type: BubbleType.sendBubble),
+                      alignment: Alignment.topRight,
+                      margin: const EdgeInsets.only(bottom: 10),
+                      backGroundColor: const Color(0xFF8754f8),
+                      child: Container(
+                        constraints: BoxConstraints(
+                          maxWidth: MediaQuery.of(context).size.width * 0.7,
                         ),
-                        Text(
-                          message,
-                          style: const TextStyle(color: Colors.white),
+                        child: Column(
+                          crossAxisAlignment: isMe
+                              ? CrossAxisAlignment.end
+                              : CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              userName,
+                              style: const TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white),
+                            ),
+                            Text(
+                              message,
+                              style: const TextStyle(color: Colors.white),
+                            ),
+                          ],
                         ),
-                      ],
+                      ),
                     ),
                   ),
-                ),
+                ],
               ),
             if (!isMe)
-              Padding(
-                padding: const EdgeInsets.fromLTRB(45, 10, 0, 0),
-                child: ChatBubble(
-                  clipper: ChatBubbleClipper8(type: BubbleType.receiverBubble),
-                  backGroundColor: const Color(0xffE7E7ED),
-                  margin: const EdgeInsets.only(bottom: 10),
-                  child: Container(
-                    constraints: BoxConstraints(
-                      maxWidth: MediaQuery.of(context).size.width * 0.7,
-                    ),
-                    child: Column(
-                      crossAxisAlignment: isMe
-                          ? CrossAxisAlignment.end
-                          : CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          userName,
-                          style: const TextStyle(
-                            fontWeight: FontWeight.bold,
-                            color: Colors.black,
-                          ),
+              Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(45, 10, 0, 0),
+                    child: ChatBubble(
+                      clipper:
+                          ChatBubbleClipper8(type: BubbleType.receiverBubble),
+                      backGroundColor: const Color(0xffE7E7ED),
+                      margin: const EdgeInsets.only(bottom: 10),
+                      child: Container(
+                        constraints: BoxConstraints(
+                          maxWidth: MediaQuery.of(context).size.width * 0.7,
                         ),
-                        Text(
-                          message,
-                          style: const TextStyle(color: Colors.black),
+                        child: Column(
+                          crossAxisAlignment: isMe
+                              ? CrossAxisAlignment.end
+                              : CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              userName,
+                              style: const TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: Colors.black,
+                              ),
+                            ),
+                            Text(
+                              message,
+                              style: const TextStyle(color: Colors.black),
+                            ),
+                          ],
                         ),
-                      ],
+                      ),
                     ),
                   ),
-                ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Text(
+                        getFormattedTime(),
+                        style: const TextStyle(
+                            fontSize: 10, color: Palette.darkGray),
+                      ),
+                    ],
+                  ),
+                ],
               )
           ],
         ),
